02b9bb2a8afe4531976860ac6c1b9a53d09fe379
KonstantinosStou <konstour@outlook.com.gr>
2024-01-22 21:45:43 +0200

    Your commit message here

--- controller.js
+++ controller.js
@@ -20,7 +20,8 @@ const serviceP = document.querySelector('.service-title-p');
 //const serviceLearnMore = document.querySelector('.service-learn-more');
 
 const servicePosterTextElements = document.querySelectorAll('.service-poster-text');
-const newTexts = ["Paintjob oven", "Calibre", "something else", "something else too"];
+const newTexts = ["Paintjob oven", "Calibre", "10 paint spaces"];
+const newTexts2 = ["Headlight defogging","Car paintjob","waxing","10 paint spaces","Painting oven","Calibre"];
 
 
 const staffTitle = document.querySelector('.staff-title');
@@ -29,6 +30,7 @@ const staffText = document.querySelector('.staff-text');
 const contactTitle = document.querySelector('.contact-title');
 const privacy = document.querySelector('.privacy');
 const companyName = document.querySelector('.cc');
+const adressSpan = document.querySelector('.address-span');
 //original html texts
 const originalValues = {
     navBar1: navBar1.innerText,
@@ -39,12 +41,13 @@ const originalValues = {
     serviceTitleText: serviceTitleText.innerText,
     serviceP: serviceP.innerText,
     //serviceLearnMore: serviceLearnMore.innerText,
-    servicePosterTextElements: Array.from(servicePosterTextElements).map(element => element.innerText),
+    servicePosterTextElements: Array.from(servicePosterTextElements).map(servicePosterText => servicePosterText.innerText),
     staffTitle: staffTitle.innerText,
     staffText: staffText.innerText,
     contactTitle: contactTitle.innerText,
     privacy: privacy.innerText,
     companyName: companyName.innerText,
+    adressSpan: adressSpan.innerText,
 
 };
 
@@ -129,13 +132,15 @@ const navbarLang = function(element) {
     serviceTitleText.innerText = originalValues.serviceTitleText;
     serviceP.innerText = originalValues.serviceP;
     //serviceLearnMore.innerText = originalValues.serviceLearnMore;
-    servicePosterTextElements.innerText = originalValues.servicePosterTextElements;
+    servicePosterTextElements.innerText = servicePosterTextElements.forEach((servicePosterText, index) => {
+      servicePosterText.innerText = originalValues.servicePosterTextElements[index];
+    });
     staffTitle.innerText = originalValues.staffTitle;
     staffText.innerText = originalValues.staffText;
     contactTitle.innerText = originalValues.contactTitle;
     privacy.innerText = originalValues.privacy;
     companyName.innerText = originalValues.companyName;
-  
+    adressSpan.innerText = originalValues.adressSpan;
     // Clear original values
     //originalValues = {};
   };
@@ -153,12 +158,14 @@ const navbarLang = function(element) {
       navBar1.innerText = 'SERVICE';
       navBar2.innerText = 'STAFF';
       navBar3.innerText = 'CONTACT';
-      liPosters.forEach(liPoster => {
-        liPoster.innerText = 'service text';
+      liPosters.forEach((element, index) => {
+        if (index < newTexts2.length) {
+            element.innerText = newTexts2[index];
+        }
       });
-      posterT.innerText = 'Poster title';
-      serviceTitleText.innerText = 'SERVICE TITLE';
-      serviceP.innerText = 'Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text '; 
+      posterT.innerText = 'BODYWORK/PAINT SHOP I&P STOURAITIS';
+      serviceTitleText.innerText = 'OUR STORE';
+      serviceP.innerText = 'In our store you will find a specialized paint and body shop area, with spaces for up to 10 cars. Our highly experienced staff aims to repair your car\'\s damage immediately.'; 
       
       servicePosterTextElements.forEach((element, index) => {
         if (index < newTexts.length) {
@@ -167,17 +174,60 @@ const navbarLang = function(element) {
       });
       
       //serviceLearnMore.innerText = 'Learn more';
-      staffTitle.innerText = 'Experienced staff';
-      staffText.innerText = 'Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text ';
+      staffTitle.innerText = 'EXPERIENCED STAFF';
+      staffText.innerText = 'With many years of experience in the fields of car bodywork and painting, our store staff is ready to serve you.';
 
-      contactTitle.innerText = 'Contact us';
-      //companyName.privacy.innerText = 'Privacy Policy';
+      contactTitle.innerText = 'CONTACT US';
+      privacy.innerText = 'Privacy Policy';
       companyName.innerText = 'I&P bodywork, All rights reserved';
+      adressSpan.innerText = 'Location:';
     }
   });
 }
 
 navbarLang(grBtnMob);
 navbarLang(enBtnMob);
-console.log(serviceTitleText.innerText);
+console.log(adressSpan.innerText);
 
+//////////
+
+const slideshow = (element, startNumber, endNumber, interval) => {
+  let currentNumber = startNumber;
+  let isForward = true; // Indicates whether the slideshow is going forward or backward
+
+  const updateImage = () => {
+      const img = element.querySelector('.staff-img');
+      img.src = `p&i staff-${currentNumber}.png`;
+
+      // Increment or decrement the currentNumber based on the direction
+      if (isForward) {
+          currentNumber++;
+      } else {
+          currentNumber--;
+      }
+
+      // Toggle direction when reaching the end or the start
+      if (currentNumber > endNumber) {
+          currentNumber = endNumber - 1;
+          isForward = false;
+      } else if (currentNumber < startNumber) {
+          currentNumber = startNumber + 1;
+          isForward = true;
+      }
+  };
+
+  // Initial update
+  updateImage();
+
+  // Set interval for slideshow
+  const intervalId = setInterval(updateImage, interval);
+
+  // Return the intervalId if you want to be able to clear the interval later
+  return intervalId;
+};
+
+// Run slideshow for the first <li> element
+const intervalId1 = slideshow(document.querySelector('.staff-list li:nth-child(1)'), 1, 6, 3000);
+
+// Run slideshow for the second <li> element
+const intervalId2 = slideshow(document.querySelector('.staff-list li:nth-child(2)'), 7, 12, 3000);
